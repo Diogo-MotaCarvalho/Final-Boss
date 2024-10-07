0fe0f01ef0c648f8dbe12fab0dba274e5806b0c9
Diogo-MotaCarvalho <diogo.carvalho@flutteruki.com>
2024-10-07 17:36:56 +0100

    Implemented Consumer Tests

--- consumer/src/test/java/port/ConsumerTest.java
+++ consumer/src/test/java/port/ConsumerTest.java
@@ -1,9 +1,34 @@
 package port;
 
+import com.finalboss.domain.MarketUpdate;
+import com.finalboss.port.Consumer;
+import com.finalboss.useCases.EventHandler;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ConsumerTest {
 
+    @Mock
+    EventHandler eventHandler;
+
+    @Mock
+    MarketUpdate marketUpdate;
+
+    Consumer victim;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        victim = new Consumer(eventHandler);
+    }
+
     @Test
-    public void consumerTest() {}
+    public void consumerTest() {
+        victim.onMessage(marketUpdate);
+        verify(eventHandler).readOperation(marketUpdate);
+    }
 }
